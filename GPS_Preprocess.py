e2d851249bfbf642c1f824c15daec5ebc48b23ee
Graham Smith <gps1539@gmail.com>
2025-11-06 14:41:25 -0800

    get object from image and use in output filename

--- GPS_Preprocess.py
+++ GPS_Preprocess.py
@@ -5,9 +5,9 @@ This script executes siril commands to calibrate, background extract(optional), 
 
 With the platesolving option the script can register mosaics.
 
-Currently the script can no be started directly from siril-cli, but it can be started from a ssf script using the 'pyscript' command i.e. pyscript GPS_Preprocess.py. Such a ssf script can run the this script several times i.e. to try different settings, with unique result files based on setting values. 
+The script can be started directly from the siril command line and can also be started from a ssf script using the 'pyscript' command i.e. pyscript GPS_Preprocess.py. Such a ssf script can run the this script several times i.e. to try different settings, with unique result files based on setting values. 
 
-The script also skips restacking master biases, flats, darks, background exaction and platesolving if they exist. 
+The script also skips restacking master biases, flats, darks, background exaction and platesolving if they already exist. 
 
 Example ssf script to run this script
 ---
@@ -26,6 +26,7 @@ import sys
 import os
 import sirilpy as s
 import argparse
+import re
 
 # command line options and help
 parser = argparse.ArgumentParser()
@@ -33,6 +34,7 @@ parser.add_argument("-b","--background", nargs='+', help="background filter sett
 parser.add_argument("-bg","--bkg", help="extract background" ,action="store_true")
 parser.add_argument("-d","--workdir", nargs='+', help="set working directory")
 parser.add_argument("-f","--feather", nargs='+', help="set feathering amount in px")
+parser.add_argument("-nc","--no_calibration", help="do not calibrate" ,action="store_true")
 parser.add_argument("-ps","--platesolve", help="platesolve" ,action="store_true")
 parser.add_argument("-r","--round", nargs='+', help="round filter settings, XX%% or X")
 parser.add_argument("-w","--wfwhm", nargs='+', help="wfwhm filter settings, XX%% or X")
@@ -41,7 +43,7 @@ args = parser.parse_args()
 
 siril = s.SirilInterface()
 
-VERSION = "0.0.6"
+VERSION = "0.0.8"
 
 # ==============================================================================
 # Prototype sirilpy preprocessing script
@@ -52,49 +54,57 @@ def master_bias(bias_dir, process_dir):
 		print('master bias exists, skipping')
 		return		
 	else:
-		siril.cmd("cd " + bias_dir )
-		siril.cmd("convert bias -out=" + process_dir)
-		siril.cmd("cd " + process_dir)
-		siril.cmd("stack bias rej 3 3  -nonorm")
+		siril.cmd(f"cd {bias_dir}")
+		siril.cmd(f"convert bias -out={process_dir}")
+		siril.cmd(f"cd  {process_dir}")
+		siril.cmd(f"stack bias rej 3 3 -nonorm")
     
 def master_flat(flat_dir, process_dir):
 	if os.path.exists ((workdir) + '/process/pp_flat_stacked.fit'):
 		print('master flat exists, skipping')
 		return		
 	else:
-		siril.cmd("cd " + flat_dir )
-		siril.cmd("convert flat -out=" + process_dir)
-		siril.cmd("cd " + process_dir)
-		siril.cmd("calibrate flat  -bias=bias_stacked")
-		siril.cmd("stack  pp_flat rej  3 3 -norm=mul")
+		siril.cmd(f"cd {flat_dir}")
+		siril.cmd(f"convert flat -out={process_dir}")
+		siril.cmd(f"cd {process_dir}")
+		siril.cmd("calibrate flat -bias=bias_stacked")
+		siril.cmd("stack pp_flat rej 3 3 -norm=mul")
     
 def master_dark(dark_dir, process_dir):
 	if os.path.exists ((workdir) + '/process/dark_stacked.fit'):
 		print('master dark exists, skipping')
 		return		
 	else:
-		siril.cmd("cd " + dark_dir )
-		siril.cmd("convert dark -out=" + process_dir)
-		siril.cmd("cd " + process_dir)
-		siril.cmd("stack dark rej 3 3 -nonorm")
+		siril.cmd(f"cd {dark_dir}")
+		siril.cmd(f"convert dark -out={process_dir}")
+		siril.cmd(f"cd {process_dir}")
+		siril.cmd(f"stack dark rej 3 3 -nonorm")
     
 def light(light_dir, process_dir):
 	if os.path.exists ((workdir) + '/process/pp_light_.seq'):
 		print('pp_light exists, skipping')
 		return		
 	else:
-		siril.cmd("cd " + light_dir)
-		siril.cmd("convert light -out=" + process_dir)
-		siril.cmd("cd " + process_dir )
-		siril.cmd("calibrate light -dark=dark_stacked -flat=pp_flat_stacked -cc=dark -cfa -equalize_cfa")
+		siril.cmd(f"cd {light_dir}")
+		siril.cmd(f"convert light -out={process_dir}")
+		siril.cmd(f"cd {process_dir}")
+		siril.cmd(f"calibrate light -dark=dark_stacked -flat=pp_flat_stacked -cc=dark -cfa -equalize_cfa")
 
+def light_nc(light_dir, process_dir):
+	if os.path.exists ((workdir) + '/process/pp_light_.seq'):
+		print('pp_light exists, skipping')
+		return		
+	else:
+		siril.cmd(f"cd {light_dir}")
+		siril.cmd(f"convert pp_light -out={process_dir}")
+		
 def bkg_extract(process_dir):
 	if os.path.exists ((workdir) + '/process/bkg_pp_light_.seq'):
 		print('background extracted, skipping')
 		return		
 	else:
-		siril.cmd("cd " + process_dir)
-		siril.cmd("seqsubsky pp_light 1 -samples=10")
+		siril.cmd(f"cd {process_dir}")
+		siril.cmd(f"seqsubsky pp_light 1 -samples=10")
 	
 def platesolve(process_dir):
 	if args.bkg:
@@ -105,21 +115,23 @@ def platesolve(process_dir):
 		if os.path.exists ((workdir) + '/process/r_pp_light_.seq'):
 			print('sequence platesolved, skipping')
 			return		
-
 	siril.cmd("cd " + process_dir)
-	siril.cmd("seqplatesolve " + light_seq + " -nocache -catalog=nomad -force -disto=ps_distortion")
-
+	siril.cmd(f"seqplatesolve {light_seq} -nocache -catalog=nomad -force -disto=ps_distortion")
+	
 def register(process_dir):
-	siril.cmd("cd " + process_dir)
+	siril.cmd(f"cd {process_dir}")
+	flat = " " if args.no_calibration else " -flat=pp_flat_stacked"
 	if args.platesolve:
-		siril.cmd("seqapplyreg " + light_seq + " -framing=max -filter-bkg=" + bkg + " -filter-round=" + roundf + " -filter-wfwhm=" + wfwhm + " -kernel=square -drizzle -scale=" + drizzle_scale + " -pixfrac=" + pix_frac + " -flat=pp_flat_stacked")
+		siril.cmd(f"seqapplyreg {light_seq} -framing=max -filter-bkg={bkg} -filter-round={roundf} -filter-wfwhm={wfwhm} -kernel=square -drizzle -scale={drizzle_scale} -pixfrac={pix_frac} {flat}")
 	else :
-		siril.cmd("register " + light_seq + " -2pass")
-		siril.cmd("seqapplyreg " + light_seq + " -filter-bkg=" + bkg + " -filter-round=" + roundf + " -filter-wfwhm=" + wfwhm + " -kernel=square -drizzle -scale=" + drizzle_scale + " -pixfrac=" + pix_frac + " -flat=pp_flat_stacked")
+		siril.cmd(f"register {light_seq} -2pass")
+		siril.cmd(f"seqapplyreg {light_seq} -filter-bkg={bkg} -filter-round={roundf} -filter-wfwhm={wfwhm} -kernel=square -drizzle -scale={drizzle_scale} -pixfrac={pix_frac} {flat}")
 
 def stack(process_dir):
-	siril.cmd("cd " + process_dir)	
-	siril.cmd("stack r_" + light_seq + " rej 3 3 -norm=addscale -output_norm -rgb_equal -maximize -filter-included -weight=wfwhm  -feather=" + feather + " -out=../result-bkg_"+bkg+"-round_"+roundf+"-wfwhm_"+wfwhm+"-drizzle_"+drizzle_scale+"-feather_"+feather)
+	siril.cmd(f"cd {process_dir}")
+	siril.cmd("load pp_light_00001.fit")
+	obj=(siril.get_image_fits_header(return_as='dict')['OBJECT']).replace(" ", "")
+	siril.cmd(f"stack r_{light_seq} rej 3 3 -norm=addscale -output_norm -rgb_equal -maximize -filter-included -weight=wfwhm  -feather={feather} -out=../{obj}_b{bkg}-r{roundf}-w{wfwhm}-z{drizzle_scale}-f{feather}")
 	siril.cmd("close")
     
 # ==============================================================================
@@ -139,10 +151,13 @@ try:
 	process_dir = '../process'
 	siril.cmd("set32bits")
 	siril.cmd("setext fit")
-	master_bias(workdir+ '/biases' ,process_dir)
-	master_flat(workdir+ '/flats'  ,process_dir)
-	master_dark(workdir+ '/darks'  ,process_dir)
-	light(workdir+ '/lights' ,process_dir)
+	if args.no_calibration:
+		light_nc(workdir+ '/lights',process_dir)
+	else:
+		master_bias(workdir+ '/biases' ,process_dir)
+		master_flat(workdir+ '/flats'  ,process_dir)
+		master_dark(workdir+ '/darks'  ,process_dir)
+		light(workdir+ '/lights' ,process_dir)
 	if args.bkg: 
 		bkg_extract(workdir+ '/process')
 		light_seq = 'bkg_pp_light'
